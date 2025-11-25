bff2f4ade906ed86f1af26a721d824125eb4ae03
Joey <joey@evolution-x.org>
2025-11-26 08:42:32 +0900

    Revert "Complete removal of device policy from UserManagerInternal"
    * Used by app lock.
    
    This reverts commit c64fa36e210504b8ec3c1b3ea961262621d6d52f.

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -2337,7 +2337,7 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
 
     private Owners makeOwners(Injector injector, PolicyPathProvider pathProvider) {
         return new Owners(
-                injector.getUserManager(),
+                injector.getUserManager(), injector.getUserManagerInternal(),
                 injector.getPackageManagerInternal(),
                 injector.getActivityTaskManagerInternal(),
                 injector.getActivityManagerInternal(), mStateCache, pathProvider);
